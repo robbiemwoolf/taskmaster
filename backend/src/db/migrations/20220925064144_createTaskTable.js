7de29205dc270f80cdb1c5ae2e8a816b07a98e24
Robbie Woolf <robbiemwoolf@gmail.com>
2022-09-25 12:39:28 -0700

    task migration table updated and seeded with new data

--- backend/src/db/migrations/20220925064144_createTaskTable.js
+++ backend/src/db/migrations/20220925064144_createTaskTable.js
@@ -1,14 +1,13 @@
 exports.up = function(knex) {
     return knex.schema.createTable('task', (table) => {
-        table.increments('supplier_id').primary()
+        table.increments('task_id').primary()
         table.string('task_description').notNullable()
         table.date('complete_by')
-        table.boolean('task_archived_brutal')
-        table.boolean('task_brutal')
-        table.boolean('task_archived_daily')
         table.boolean('task_daily')
-        table.boolean('task_archived_longterm')
-        table.boolean('task_longterm')
+        table.boolean('task_habit')
+        table.boolean('task_brutal')
+        table.boolean('task_complete')
+        table.date('date_completed')
         table.timestamps(true, true)
     })
 }

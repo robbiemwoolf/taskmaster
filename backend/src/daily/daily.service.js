e1af7486b38f03a6701063aa973b5e1549d5a547
Robbie Woolf <robbiemwoolf@gmail.com>
2022-09-25 13:16:12 -0700

    updated daily list functions to reflect updated db

--- backend/src/daily/daily.service.js
+++ backend/src/daily/daily.service.js
@@ -4,15 +4,22 @@ function create(task) {
     return knex('task')
         .insert(task)
         .returning('task_description')
-        .then((createdTasks) => createdTasks)
+        .then((createdTasks) => createdTasks[0])
 }
 
 function list() {
-    return knex('task').select('task_description').where({ task_daily: true })
+    return knex('task')
+        .select('task_description')
+        .where({ task_daily: true, task_complete: false })
+        .then((tasks) => tasks)
 }
 
 function listArchived() {
-    return knex('task').select('task_description').where({ task_archived_daily: true })
+    return knex('task')
+        .orderBy('date_completed', 'desc')
+        .select('task_description', 'date_completed')
+        .where({ task_daily: true, task_complete: true })
+        .then((tasks) => tasks)
 }
 
 module.exports = {
